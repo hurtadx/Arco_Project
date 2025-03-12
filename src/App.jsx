9301c7420fc645162241dc0a695a36018f868905
Hurtadx <andreshm253@gmail.com>
2025-03-12 16:23:52 -0500

    feat: implement layout and tab navigation for object and equipment changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,44 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Navbar from './components/common/Navbar'
+import ObjectChangeForm from './components/ObjectChange/ObjectChangeForm'
+import ObjectChangeList from './components/ObjectChange/ObjectChangeList'
+import EquipmentForm from './components/EquipmentChange/EquipmentForm'
+import EquipmentList from './components/EquipmentChange/EquipmentList'
+import EquipmentChangeForm from './components/EquipmentChange/EquipmentChangeForm'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [activeTab, setActiveTab] = useState('objectChanges')
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="app-container">
+      <Navbar activeTab={activeTab} setActiveTab={setActiveTab} />
+      
+      <main className="content">
+        {activeTab === 'objectChanges' && (
+          <div className="object-changes-container">
+            <h2>Registro de Cambio de Objetos</h2>
+            <ObjectChangeForm />
+            <ObjectChangeList />
+          </div>
+        )}
+        
+        {activeTab === 'equipmentRegistry' && (
+          <div className="equipment-registry-container">
+            <h2>Registro de Equipos</h2>
+            <EquipmentForm />
+            <EquipmentList />
+          </div>
+        )}
+        
+        {activeTab === 'equipmentChanges' && (
+          <div className="equipment-changes-container">
+            <h2>Registro de Cambio de Equipos</h2>
+            <EquipmentChangeForm />
+          </div>
+        )}
+      </main>
+    </div>
   )
 }
 
