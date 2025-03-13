b5af6abd26856f17fe65812c2987049c3a5d85dd
Hurtadx <andreshm253@gmail.com>
2025-03-13 09:39:58 -0500

    feat: add Font Awesome icon library integration for enhanced UI components

---
+++ src/utils/fontAwesome.js
@@ -0,0 +1,23 @@
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { 
+  faClipboardList, faLaptop, faExchange, 
+  faPlus, faSearch, faFileExport, 
+  faPencilAlt, faTrash, faSort,
+  faChevronRight, faChevronLeft, faCalendarAlt,
+  faUser, faBuilding, faBox, faCheck,
+  faArrowRight, faSave, faUndo, faRedo
+} from '@fortawesome/free-solid-svg-icons';
+import { 
+  faClipboard, faBell, faQuestionCircle 
+} from '@fortawesome/free-regular-svg-icons';
+
+
+library.add(
+  faClipboardList, faLaptop, faExchange,
+  faPlus, faSearch, faFileExport,
+  faPencilAlt, faTrash, faSort,
+  faChevronRight, faChevronLeft, faCalendarAlt,
+  faUser, faBuilding, faBox, faCheck,
+  faArrowRight, faSave, faUndo, faRedo,
+  faClipboard, faBell, faQuestionCircle
+);
