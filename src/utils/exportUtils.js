2e16ce133fe16299e9c8f450c8de96dbc1bcde78
Hurtadx <andreshm253@gmail.com>
2025-03-13 11:16:49 -0500

    feat: add exportToExcel utility function for data export with placeholder alert

---
+++ src/utils/exportUtils.js
@@ -0,0 +1,5 @@
+
+export const exportToExcel = (data, type = 'data') => {
+  console.log(`Exportando ${type} a Excel:`, data);
+  alert(`Esta función exportará ${data.length} registros en la versión final de la aplicación`);
+};
